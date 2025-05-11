a31d2d31bfcc189584b3fb93ae07d8168984d02d
UbiraciOliveira <ubira.domingos@gmail.com>
2025-05-11 20:50:34 -0300

    [Ubiraci] Adicionado resto da divisão.

--- CalculadoraSimples/Program.cs
+++ CalculadoraSimples/Program.cs
@@ -3,8 +3,8 @@ Console.WriteLine("Digite 1 para adição +");
 Console.WriteLine("Digite 2 para Subtração -");
 Console.WriteLine("Digite 3 para Multiplicação *");
 Console.WriteLine("Digite 4 para Divisão /");
-int
- operacao = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Digite 5 para o resto da divisão %");
+int operacao = int.Parse(Console.ReadLine()!);
 
 Console.Write("Digite o primeiro número ");
 int numero1 = int.Parse(Console.ReadLine()!);
@@ -20,10 +20,11 @@ switch (operacao)
     case 2: { resultado = numero1 - numero2; break; }
     case 3: { resultado = numero1 * numero2; break; }
     case 4: { resultado = numero1 / numero2; break; }
+    case 5: { resultado = numero1 % numero2; break; }
     default: Console.WriteLine("Opeção invalida!"); break;
 };
 
 Console.WriteLine($"Resultado {resultado}");
-
+        
 
 
